4b676ab732cc000b0c81ecf0a970e3c0993c2d5a
leonardobarone <leonardobarone26@gmail.com>
2021-09-10 16:14:40 +0200

    risolto con filter, anche se troppo lungo

--- snack3/js/main.js
+++ snack3/js/main.js
@@ -2,27 +2,45 @@
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
 
-function funzione(arr, min, max){
+                                // const funzione = (arr, min, max) => {
 
-    let newArr = [];
+                                //     let newArr = [];
+                                    
+                                //     arr.forEach(
+                                //         (element, index) => {
+                                    
+                                //             if (index + 1 >= min && index + 1 <= max){
+                                //                 newArr.push(element);
+                                //             }
+                                    
+                                //         }
+                                //     );
+                                    
+                                //     return newArr;
+                                // }
+
+                                // let arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "l"];
+                                // console.log(funzione(arr, 1, 8));
+
+
+// PROVO A FARLO CON FILTER
+
+
+function numeriCompresi(arr, min, max){
+
+    const compresi = arr.filter((element, index) => {
     
-    arr.forEach(
-        (element, index) => {
-
-            if (index + 1 >= min && index + 1 <= max){
-                newArr.push(element);
-            }
-
+        if (index >= min && index <= max){
+            return element;
         }
+    
+    }
+    
     );
-
-    return newArr;
-
+    return compresi;
 }
 
 
+const arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "l"];
 
-
-let arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "l"];
-
-console.log(funzione(arr, 1, 8));
+console.log(numeriCompresi(arr, 3, 5));
